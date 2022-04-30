28d3f5702536e7fc6e50c5a5ff7b0b9d3efb18f3
Martyin Máté <matemartyin@gmail.com>
2022-04-30 19:09:17 +0200

    Init project

---
+++ src/test/java/com/matemartyin/crypt/CryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matemartyin.crypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
